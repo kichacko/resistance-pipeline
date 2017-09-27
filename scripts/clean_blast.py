ae04c4f16f7f962b2dbfa8b8ae3959ebb8030513
Kieran <kieran.chacko@icahn.mssm.edu>
2017-09-27 17:18:24 -0400

    Update clean_blast.py

--- scripts/clean_blast.py
+++ scripts/clean_blast.py
@@ -15,25 +15,25 @@ import os
 firstarg=sys.argv[1]
 
 # Load homolog hits
-df = pd.read_csv('./tmp-files/blastdb/' + str( firstarg ) + '.out',
-                  sep = "\t",
-                  header = None)
+df = pd.read_csv('./tmp-files/' + str(firstarg) + '/' + str( firstarg ) + '.blast',
+                 sep = "\t",
+                 header = None)
 
 # Relabel the columns
 df.columns = [
-    "Contig",
-    "ID",
-    "Percent_ID",
-    "Length",
-    "Mismatch",
-    "Gap_Open",
-    "Query_Start",
-    "Query_End",
-    "Subject_Start",
-    "Subject_End",
-    "E_Value",
-    "Bitscore"
-    ]
+              "Contig",
+              "ID",
+              "Percent_ID",
+              "Length",
+              "Mismatch",
+              "Gap_Open",
+              "Query_Start",
+              "Query_End",
+              "Subject_Start",
+              "Subject_End",
+              "E_Value",
+              "Bitscore"
+              ]
 
 # Sort the data by percent identity
 df = df.sort_values(by = ['Percent_ID'])
@@ -41,12 +41,13 @@ df = df.loc[df['Percent_ID'] >= 90]
 df = df.loc[df['E_Value'] <= 1e-10]
 df = df.drop_duplicates(subset = ['ID'], keep = 'last')
 
-# Write outputs
+
 if firstarg == "homolog":
-    df = df.loc[:, ['ID', 'Contig']]
-    df['Type'] = "Homolog"
-    df.to_csv('./tmp-files/blastdb/' +str( firstarg ) + '.hits', sep='\t', index=False, header=False)
-    
+    df = df.loc[:, ['ID', 'Contig', 'Percent_ID']]
+    df.to_csv('./tmp-files/homolog/' +str( firstarg ) + '.hom', sep='\t', index=False, header=False)
+
 elif firstarg == "variant":
-    df = df.loc[:, ['Contig', 'ID','Query_Start','Query_End']]
-    df.to_csv('./tmp-files/blastdb/' +str( firstarg ) + '.hits', sep='\t', index=False, header=False)
+    df = df.loc[:, ['ID', 'Contig']]
+    df.to_csv('./tmp-files/variant/' +str( firstarg ) + '.hits', sep='\t', index=False, header=False)
+
+
