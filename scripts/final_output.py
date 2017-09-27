ec3712700ee0e41f3dd3ed3a993920f8eb110ac2
Kieran <kieran.chacko@icahn.mssm.edu>
2017-09-27 17:14:05 -0400

    Update final_output.py

--- scripts/final_output.py
+++ scripts/final_output.py
@@ -7,36 +7,37 @@
 #
 
 # Import Modules
+
 import pandas as pd
 import sys
 import os
 
-# Load homolog.hits
-if os.stat("./tmp-files/blastdb/homolog.hits").st_size > 0:
-    df = pd.read_csv('./tmp-files/blastdb/homolog.hits',
-                      sep = "\t",
-                      quotechar='"',
-                      header = None)
+# Load variant.nucdiff
+if os.stat("./tmp-files/homolog/homolog.hom").st_size > 0:
+    df = pd.read_csv('./tmp-files/homolog/homolog.hom',
+                     sep = "\t",
+                     header = None)
 else:
     df = pd.DataFrame()
 
-# Load variants.output
-if os.stat("./tmp-files/nucdiff/variants.output").st_size > 0:
-    df2 = pd.read_csv('./tmp-files/nucdiff/variants.output',
+# Load variant.database
+if os.stat("./tmp-files/variant/variant.var").st_size > 0:
+    df2 = pd.read_csv('./tmp-files/variant/variant.var',
                       sep = "\t",
                       header = None)
 else:
     df2 = pd.DataFrame()
 
-# Combine variant and homolog hits together
+# Combine outputs together
 output = pd.concat([df, df2], ignore_index = True)
 
 # Relabel the columns
 output.columns = [
-    "ID",
-    "PROKKA_ID",
-    "Variant_Type"
-    ]
+                  "ID",
+                  "Contig",
+                  "Variant_Type"
+                  ]
 
-# Write output
 output.to_csv('./out/output.txt', sep='\t', index=False)
+
+
