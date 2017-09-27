0ac67e257e4ed5dc55c1bc02167ee937e03d4a11
Kieran <kieran.chacko@icahn.mssm.edu>
2017-09-27 17:19:12 -0400

    Update run_blast.sh

--- scripts/run_blast.sh
+++ scripts/run_blast.sh
@@ -22,27 +22,44 @@ module load python
 module load py_packages
 
 ##############
-# BLAST Homolog
+# Homolog
 ##############
 
-echo "Making blastdb"
+echo -e "\n...Making Blast database for homologs. \n"
 makeblastdb -dbtype 'prot' -in "./tmp-files/blastdb/homolog.faa" -input_type 'fasta' -out "./tmp-files/blastdb/homolog"
 
-echo "Running Homolog Blast"
-blastp -db "./tmp-files/blastdb/homolog" -query "./tmp-files/prokka/prokka.faa" -outfmt 6 -max_target_seqs 1 -out "./tmp-files/blastdb/homolog.out"
+echo -e "\n...Running Blast for homologs. \n"
+blastp -db "./tmp-files/blastdb/homolog" -query "./prokka/prokka.faa" -outfmt 6 -max_target_seqs 1 -out "./tmp-files/homolog/homolog.blast"
 
-echo "Cleaning Blast Result"
+echo -e "\n...Cleaning Blast Result. \n"
 python ./scripts/clean-blast.py homolog
 
 ##############
-# BLAST variant
+# Variant
 ##############
 
-echo "Making blastdb"
-makeblastdb -dbtype 'nucl' -in "./tmp-files/blastdb/variant.fna"  -input_type 'fasta' -out "./tmp-files/blastdb/variant"
+echo -e "\n...Making Blast database for variants. \n"
+makeblastdb -dbtype 'prot' -in "./tmp-files/blastdb/variant.faa"  -input_type 'fasta' -out "./tmp-files/blastdb/variant"
 
-echo "Running Variant Blast"
-blastn -db "./tmp-files/blastdb/variant" -query "./tmp-files/prokka/prokka.fna" -outfmt 6 -max_target_seqs 1 -out "./tmp-files/blastdb/variant.out"
+echo -e "\n...Running Blast for variants. \n"
+blastp -db "./tmp-files/blastdb/variant" -query "./prokka/prokka.faa" -outfmt 6 -max_target_seqs 1 -out "./tmp-files/variant/variant.blast"
 
-echo "Cleaning Blast Result"
+echo -e "\n...Cleaning Blast Result. \n"
 python ./scripts/clean-blast.py variant
+# Output = .hits
+
+echo -e "\n...Finding variants from Blast. \n"
+while read i j k
+do
+
+    python ./scripts/extract_seqs.py "${j}"
+# Output = PROKKA.faa
+    blastp -subject "./tmp-files/variant/${i}.faa" -query "./tmp-files/variant/${j}.faa" -outfmt 5 -out "./tmp-files/variant/${i}.blast"
+# OUTPUT = Ef1.blast
+    python ./scripts/extract_blast_var.py "./tmp-files/variant/${i}.blast"
+# OUTPUT = Ef1.out
+
+
+done <  "./tmp-files/variant/variant.hits"
+
+cat "./tmp-files/variant/"*.out >> "./tmp-files/variant/variant.blastvar"
