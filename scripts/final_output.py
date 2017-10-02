1902c4a6dfc6a2c8fccf9ed78b6479998a41f6d6
Kieran <kieran.chacko@icahn.mssm.edu>
2017-10-02 15:16:31 -0400

    Update final_output.py

--- scripts/final_output.py
+++ scripts/final_output.py
@@ -7,29 +7,39 @@
 #
 
 # Import Modules
-
 import pandas as pd
 import sys
 import os
 
-# Load variant.nucdiff
-if os.stat("./tmp-files/homolog/homolog.hom").st_size > 0:
-    df = pd.read_csv('./tmp-files/homolog/homolog.hom',
+# Load arguments
+firstarg=sys.argv[1]
+
+# Load homolog hits
+if os.stat('./' + str(firstarg) + '/tmp-files/homolog/homolog.hom').st_size > 0:
+    df = pd.read_csv('./' + str(firstarg) + '/tmp-files/homolog/homolog.hom',
                      sep = "\t",
                      header = None)
 else:
     df = pd.DataFrame()
 
-# Load variant.database
-if os.stat("./tmp-files/variant/variant.var").st_size > 0:
-    df2 = pd.read_csv('./tmp-files/variant/variant.var',
+# Load variant hits
+if os.stat('./' + str(firstarg) + '/tmp-files/variant/variant.var').st_size > 0:
+    df2 = pd.read_csv('./' + str(firstarg) + '/tmp-files/variant/variant.var',
                       sep = "\t",
                       header = None)
 else:
     df2 = pd.DataFrame()
 
+# Load rRNA hits
+if os.stat('./' + str(firstarg) + '/tmp-files/rRNA/rRNA.var').st_size > 0:
+    df3 = pd.read_csv('./' + str(firstarg) + '/tmp-files/rRNA/rRNA.var',
+                      sep = "\t",
+                      header = None)
+else:
+    df3 = pd.DataFrame()
+
 # Combine outputs together
-output = pd.concat([df, df2], ignore_index = True)
+output = pd.concat([df, df2, df3], ignore_index = True)
 
 # Relabel the columns
 output.columns = [
@@ -38,6 +48,4 @@ output.columns = [
                   "Variant_Type"
                   ]
 
-output.to_csv('./out/output.txt', sep='\t', index=False)
-
-
+output.to_csv('./' + str(firstarg) + '/out/' + str(firstarg) + '.txt', sep='\t', index=False)
