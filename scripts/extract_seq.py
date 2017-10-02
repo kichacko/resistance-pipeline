01ccda9dcf0ecd36bd53531e183ed07c5731fb85
Kieran <kieran.chacko@icahn.mssm.edu>
2017-10-02 15:15:18 -0400

    Update extract_seq.py

--- scripts/extract_seq.py
+++ scripts/extract_seq.py
@@ -7,6 +7,7 @@
 #
 
 # Import Modules
+
 import os
 import sys
 import pandas as pd
@@ -14,15 +15,28 @@ from Bio import SeqIO
 
 # Load arguments
 firstarg=sys.argv[1]
+seconarg=sys.argv[2]
+thirdarg=sys.argv[3]
 
 # Load fasta file
-for rec in SeqIO.parse('./prokka/prokka.faa', 'fasta'):
+if seconarg != "rRNA":
+    for rec in SeqIO.parse('./' + str(thirdarg) + '/prokka/prokka.faa', 'fasta'):
     
-    name = rec.id
-    seq = rec.seq
+        name = rec.id
+        seq = rec.seq
     
-    # Match the sequence and contig name
-    if name == firstarg:
-        output = open('./tmp-files/variant/' + str( name ) + '.faa', "w")
-        output.write(">" + name + "\n" + str(seq))
+        # Match the sequence and contig name
+        if name == firstarg:
+            output = open('./' + str(thirdarg) + '/tmp-files/' + str(seconarg) + '/' + str( name ) + '.faa', "w")
+            output.write(">" + name + "\n" + str(seq))
 
+if seconarg == "rRNA":
+    for rec in SeqIO.parse('./' + str(thirdarg) + '/prokka/prokka.rRNA', 'fasta'):
+        
+        name = rec.id
+        seq = rec.seq
+        
+        # Match the sequence and contig name
+        if name == firstarg:
+            output = open('./' + str(thirdarg) + '/tmp-files/' + str(seconarg) + '/' + str( name ) + '.fna', "w")
+            output.write(">" + name + "\n" + str(seq))
