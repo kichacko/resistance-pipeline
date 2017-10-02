d5fd6f56075b2cdd09f802027ea1da862da89728
Kieran <kieran.chacko@icahn.mssm.edu>
2017-10-02 15:54:59 -0400

    Update match_variants.py

--- scripts/match_variants.py
+++ scripts/match_variants.py
@@ -1,7 +1,7 @@
 #!/bin/py
 
 #  match_variants.py
-#  
+#
 #
 #  Created by Kieran Chacko on 9/20/17.
 #
@@ -15,28 +15,35 @@ import os
 firstarg=sys.argv[1]
 seconarg=sys.argv[2]
 
-# Load variant.nucdiff
-df = pd.read_csv('./' + str(seconarg) + '/tmp-files/' + str(firstarg) + '/' + str(firstarg) + '.blastvar',
-                 sep = "\t",
-                 header = None)
+# Pass if there are no variants
+if os.stat('./' + str(seconarg) + '/tmp-files/' + str(firstarg) + '/' + str(firstarg) + '.blastvar').st_size == 0:
+    df = pd.DataFrame()
+    df.to_csv('./' + str(seconarg) + '/tmp-files/' + str(firstarg) + '/' + str(firstarg) + '.var', sep='\t', index=False, header=False)
 
-# Load variant.database
-df2 = pd.read_csv('./database/variants.txt',
-                  sep = "\t",
-                  header = None)
+if os.stat('./' + str(seconarg) + '/tmp-files/' + str(firstarg) + '/' + str(firstarg) + '.blastvar').st_size > 0:
+    
+    # Load variant.nucdiff
+    df = pd.read_csv('./' + str(seconarg) + '/tmp-files/' + str(firstarg) + '/' + str(firstarg) + '.blastvar',
+                     sep = "\t",
+                     header = None)
 
-# Relabel the columns
-df.columns = [
-              "ID",
-              "Contig",
-              "Codon"
-              ]
+    # Load variant.database
+    df2 = pd.read_csv('./database/variants.txt',
+                      sep = "\t",
+                      header = None)
 
-df2.columns = [
-               "ID",
-               "Codon"
-               ]
+    # Relabel the columns
+    df.columns = [
+                  "ID",
+                  "Contig",
+                  "Codon"
+                  ]
 
-df = pd.merge(df, df2, on=['ID', 'Codon'], how='inner')
-df = df.loc[:, ['ID','Contig','Codon']]
-df.to_csv('./' + str(seconarg) + '/tmp-files/' + str(firstarg) + '/' + str(firstarg) + '.var', sep='\t', index=False, header=False)
+    df2.columns = [
+                   "ID",
+                   "Codon"
+                   ]
+
+    df = pd.merge(df, df2, on=['ID', 'Codon'], how='inner')
+    df = df.loc[:, ['ID','Contig','Codon']]
+    df.to_csv('./' + str(seconarg) + '/tmp-files/' + str(firstarg) + '/' + str(firstarg) + '.var', sep='\t', index=False, header=False)
