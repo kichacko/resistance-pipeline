8f431b6e1e0e6b4351d245e90d870730505de241
Kieran <8472595+kichacko@users.noreply.github.com>
2018-11-13 13:18:07 -0500

    Update final_output.py
    Merges output with prokka.gff_clean to annotate determinants

--- scripts/final_output.py
+++ scripts/final_output.py
@@ -1,7 +1,7 @@
 #!/bin/py
 
 #  final_output.py
-#  
+#
 #
 #  Created by Kieran Chacko on 9/20/17.
 #
@@ -38,15 +38,42 @@ if os.stat('./' + str(firstarg) + '/tmp-files/rRNA/rRNA.var').st_size > 0:
 else:
     df3 = pd.DataFrame()
 
+# Load gff_clean file
+if os.stat('./' + str(firstarg) + '/prokka/prokka.gff_clean').st_size > 0:
+    df4 = pd.read_csv('./' + str(firstarg) + '/prokka/prokka.gff_clean',
+                      sep = "\t",
+                      header = None)
+
+else:
+    print ("Error with your gff file")
+
 # Combine outputs together
 output = pd.concat([df, df2, df3], ignore_index = True)
 
-# Relabel the columns
 output.columns = [
                   "ID",
-                  "Contig",
+                  "PROKKA_ID",
                   "Variant_Type",
                   "Percent_Aligned"
                   ]
 
+# Annotate the output
+df4.columns = [
+               "Feature",
+               "Prodigal",
+               "CDS",
+               "Start",
+               "Stop",
+               ".",
+               "Strand",
+               "0",
+               "PROKKA_ID",
+               "NOTES",
+               "DESCRIPTION"
+               ]
+
+output = pd.merge(output, df4, how = 'left', on = "PROKKA_ID")
+output = output[["ID", "PROKKA_ID", "Feature", "Start", "Stop", "Variant_Type", "DESCRIPTION"]]
+
+# Output
 output.to_csv('./' + str(firstarg) + '/out/' + str(firstarg) + '.txt', sep='\t', index=True)
